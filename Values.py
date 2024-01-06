abfb2c3786d1181dc6e3bed3405fc74f248e4008
YhgzXxfz <qingfeiyou@gmail.com>
2024-01-06 22:01:37 +0000

    feat(Value): implement backward function for operations

--- Values.py
+++ Values.py
@@ -7,18 +7,40 @@ class Value:
         self.label = label
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
 
     def __repr__(self) -> str:
-        return f"Value(data={self.data})"
+        return f"Value(data={self.data}, grad={self.grad})"
 
     def __add__(self, other):
-        return Value(self.data + other.data, _children=(self, other), _op="+")
+        out = Value(self.data + other.data, _children=(self, other), _op="+")
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+
+        return out
 
     def __mul__(self, other):
-        return Value(self.data * other.data, _children=(self, other), _op="*")
+        out = Value(self.data * other.data, _children=(self, other), _op="*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
 
     def tanh(self):
         data = (math.exp(2 * self.data) - 1) / (math.exp(2 * self.data) + 1)
-        return Value(data, _children=(self,), _op="tanh")
+        out = Value(data, _children=(self,), _op="tanh")
+
+        def _backward():
+            self.grad += (1 - data**2) * out.grad
+
+        out._backward = _backward
+        return out
