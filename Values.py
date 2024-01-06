0fe0326fa400313f03aeebfb19fefa687136dc8d
YhgzXxfz <qingfeiyou@gmail.com>
2024-01-06 20:54:31 +0000

    feat(Values): add labels and visualisation

--- Values.py
+++ Values.py
@@ -1,12 +1,18 @@
+import typing as tp
+
+
 class Value:
-    def __init__(self, data) -> None:
+    def __init__(self, data, label="", _children: tp.Tuple = (), _op: str = "") -> None:
+        self.label = label
         self.data = data
+        self._prev = set(_children)
+        self._op = _op
 
     def __repr__(self) -> str:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
-        return Value(self.data + other.data)
+        return Value(self.data + other.data, _children=(self, other), _op="+")
 
     def __mul__(self, other):
-        return Value(self.data * other.data)
+        return Value(self.data * other.data, _children=(self, other), _op="*")
