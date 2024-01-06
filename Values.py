218dc16b29a94eacfcfd7f55037f0e991082c0f6
YhgzXxfz <qingfeiyou@gmail.com>
2024-01-06 22:58:17 +0000

    feat(Value): add more elementary operations

--- Values.py
+++ Values.py
@@ -15,6 +15,7 @@ class Value:
         return f"Value(data={self.data}, grad={self.grad})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, _children=(self, other), _op="+")
 
         def _backward():
@@ -26,6 +27,7 @@ class Value:
         return out
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, _children=(self, other), _op="*")
 
         def _backward():
@@ -34,6 +36,47 @@ class Value:
 
         out._backward = _backward
         return out
+
+    def exp(self):
+        out = Value(math.exp(self.data), _children=(self,), _op="exp")
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Only supports int or float."
+
+        out = Value(self.data**other, _children=(self,), _op=f"**{other}")
+
+        def _backward():
+            self.grad = (other * self.data ** (other - 1)) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __neg__(self):  # -self
+        return self * -1
+
+    def __radd__(self, other):  # other + self
+        return self + other
+
+    def __sub__(self, other):  # self - other
+        return self + (-other)
+
+    def __rsub__(self, other):  # other - self
+        return other + (-self)
+
+    def __rmul__(self, other):  # other * self
+        return self * other
+
+    def __truediv__(self, other):  # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other):  # other / self
+        return other * self**-1
 
     def tanh(self):
         data = (math.exp(2 * self.data) - 1) / (math.exp(2 * self.data) + 1)
